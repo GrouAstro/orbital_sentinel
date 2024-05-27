f9de24845bf8fcf3bfa8085effa59f5fb4a98b23
roman <rollivier@rfence.fr>
2024-05-27 08:38:03 +0200

    add class for target

--- orbital_sentinel/tracker/robot_structs/target.py
+++ orbital_sentinel/tracker/robot_structs/target.py
@@ -1,8 +1,55 @@
 from skyfield.api import EarthSatellite, load, wgs84, Topos
+#from orbital_sentinel.tracker.robot_structs.tracker import Tracker
 import math as m
+import types
 
 
-class Orbital_target:
+class Orbital_target_natural:
+
+    def __init__(self, id_body: str):
+        self.ts = load.timescale()
+        self.id = id_body
+        self.visibility = 0
+        self.velocity = 0
+        self.azimuth = 0
+        self.elevation = 0
+        self.distance = 0
+        self.target_mod = 0
+        self.type_name = 'init'
+        self.ephemeris = load('de421.bsp')
+
+    def compute_position(self, lat, lon, alt):
+
+        t = self.ts.now()
+        eph_target = self.ephemeris[self.id]
+        current_location = self.ephemeris['earth'] + wgs84.latlon(lat, lon, elevation_m=alt)
+        astro = current_location.at(t).observe(eph_target)
+        app = astro.apparent()
+
+        alt, az, distance = app.altaz()
+        self.azimuth = az.degrees
+        self.elevation = alt.degrees
+        self.distance = distance
+
+        if alt.degrees > 0:
+            self.visibility = 1
+        else:
+            self.visibility = 0
+
+    def get_azimuth(self):
+        print(self.azimuth)
+
+    def get_elevation(self):
+        print(self.elevation)
+
+    def get_visibility(self):
+        if self.visibility == 1:
+            print('Target in the sky')
+        else:
+            print('Target not available')
+
+
+class Orbital_target_earth:
 
     def __init__(self, tle_info):
         self.ts = load.timescale()
